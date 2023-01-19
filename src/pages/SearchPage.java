5db75b41d36706d752d3f10d8265a2bdd9374153
MaqboolHasan07 <maqboolhasanshaik2000@gmail.com>
2023-01-19 17:03:20 +0530

    Deleted/added/updated files

--- src/pages/SearchPage.java
+++ src/pages/SearchPage.java
@@ -1,11 +1,9 @@
 package pages;
 
-public class HomePage {
+public class SearchPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("das");
-	
 
 	}
 
